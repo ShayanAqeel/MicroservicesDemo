41983395f009cc268488b6c4409571d157b773a4
shayan <shayanaqeel.sa@gmail.com>
2024-05-11 18:15:40 +0500

    Added comment for attributes

--- ProductMicroservice/Models/AddProductRequest.cs
+++ ProductMicroservice/Models/AddProductRequest.cs
@@ -2,6 +2,7 @@
 {
     public class AddProductRequest
     {
+        //Attributes for api request
         public string name { get; set; }
         public string description { get; set; }
         public int price { get; set; }
